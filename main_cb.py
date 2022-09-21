6afa0abba40ae73a77761bc1bb914aa4358d349b
Jorge Melo <jorgeemelo7@gmail.com>
2022-09-21 07:32:47 -0300

    UPDATE

--- main_cb.py
+++ main_cb.py
@@ -10,15 +10,40 @@ bot = ChatBot('Mark 01',
         'chatterbot.logic.BestMatch', 'chatterbot.logic.MathematicalEvaluation']
 )
 
-conversa = ChatterBotCorpusTrainer(bot)
-conversa.train('chatterbot.corpus.portuguese')
+# conversa = ChatterBotCorpusTrainer(bot)
+# conversa.train('chatterbot.corpus.portuguese')
 
 bot = ChatBot('Mark 01')
 
-conversa = ListTrainer(bot)
+# conversa = ListTrainer(bot)
+'''
 conversa.train([
-# BLOCO DE CONVERSA USUARIO E BOT
+    'Oi?', 
+    'Eae, tudo certo?',
+    'Qual o seu nome?', 
+    'Mark! Seu amigo bot!',
+    'Por que seu nome é Mark?', 
+    'Mark é meu nome, sou um chatbot criado para diversão.',
+    'Prazer em te conhecer', 
+    'Igualmente meu querido',
+    'Quantos anos você tem?', 
+    'Eu nasci em 2022...',
+    'Você gosta de videogame?', 
+    'Eu sou um bot, eu só apelo.',
+    'Qual a sua bebida favorita?', 
+    'Eu bebo café, o motor de todos os programas de computador.',
+    'Qual o seu gênero?', 
+    'Sou um chatbot e gosto de algoritmos',
+    'Hahahaha',
+    'kkkk',
+    'kkk',
+    'kkkk',
+    'Conhece a Siri?',
+    'Conheço, a gente saiu por um tempo.',
+    'Conhece a Alexa?',
+    'Ela nunca deu bola pra mim.',
     ])
+'''
 
 while True:
     try:
@@ -29,4 +54,5 @@ while True:
             print("Mark 01: Não sei o que dizer sobre isso...")
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
+    
     
